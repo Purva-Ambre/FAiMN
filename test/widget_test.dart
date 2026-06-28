e1a586d1cb107d7b1eba60b59470971d28394ad6
TanishaMaji <145488395+TanishaMaji@users.noreply.github.com>
2026-06-28 16:12:04 +0530

    Delete test directory

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:heltech/main.dart';
-
-void main() {
-  testWidgets('App launches', (WidgetTester tester) async {
-    // Firebase needs real init, so just verify widget builds
-    expect(true, isTrue);
-  });
-}
